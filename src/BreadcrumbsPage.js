d44e66c013b058829a4bdacd1e19df4eaed8c475
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-03-30 21:21:29 +0800

    added the breadcrumbs component

---
+++ src/BreadcrumbsPage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { MDBContainer, MDBView, MDBMask, MDBBreadcrumb, MDBBreadcrumbItem } from 'mdbreact'
+import bgcrumbs from './img/bcrumbs-bg.jpg'
+import { Link } from 'react-router-dom'
+
+const BreadcrumbsPage = (props) =>{
+    return(
+        <div className="bcrumbs-size" >
+                <MDBMask overlay="black-strong" className="flex-center flex-column text-white text-center">
+                    <div className="brc-img">
+                            <div className="brc-content">
+                                <div className="brc-ins-cont">
+                                    <h1 className="brc-header">{props.bheader}</h1>
+                                    <a className='brc-bread-link' href="/">Home</a> / <span className="grey-text">{props.bcurrent}</span>
+                                </div>
+                            </div>
+                    </div>
+                        
+                </MDBMask>   
+            
+            
+            
+        </div>
+
+
+    )
+}
+export default BreadcrumbsPage
