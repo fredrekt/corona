6ce532907aacedef269b27dbd72a8566e8c33ce2
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-03-30 19:06:34 +0800

    done making footer

--- src/FooterPage.js
+++ src/FooterPage.js
@@ -7,26 +7,30 @@ const FooterPage = () => {
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            <h5 className="title">Corona Virus Country Tracker</h5>
             <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+            Coronavirus disease (COVID-19) is an infectious disease caused by a new virus.
+            The disease causes respiratory illness (like the flu) with symptoms such as a cough,
+            fever, and in more severe cases, difficulty breathing. 
+            Coronavirus disease spreads primarily through contact with an infected person when 
+            they cough or sneeze. It also spreads when a person touches a surface or object that 
+            has the virus on it, then touches their eyes, nose, or mouth.
             </p>
           </MDBCol>
           <MDBCol md="6">
-            <h5 className="title">Links</h5>
+            <h5 className="title">Site Development</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+                <a href="#!">Material Design Bootstrap</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a href="#!">Hosting</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
+                <a href="#!">ReactJS</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 4</a>
+                <a href="#!">API</a>
               </li>
             </ul>
           </MDBCol>
@@ -34,7 +38,7 @@ const FooterPage = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; {new Date().getFullYear()} Copyright <a href="https://www.facebook.com/fredgaringo"> Fred Garingo </a>
         </MDBContainer>
       </div>
     </MDBFooter>
