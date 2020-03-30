fdb365340244afa5b6de126cf6c2ba39dd8ecbbc
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-03-30 19:08:07 +0800

    navbar done

--- src/NavbarPage.js
+++ src/NavbarPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, 
-  MDBNavItem, MDBNavLink, MDBMask, MDBView,
+  MDBNavItem, MDBNavLink, MDBMask, MDBView, MDBBtn,
   MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBCarouselCaption
 } from 'mdbreact';
 
@@ -35,10 +35,10 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
                     <MDBNavLink to="/">Home</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">About</MDBNavLink>
+                    <MDBNavLink to="/about">About</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                    <MDBNavLink to="#">Contact</MDBNavLink>
+                    <MDBNavLink to="/contact">Contact</MDBNavLink>
                   </MDBNavItem>
                 </MDBNavbarNav>
               </MDBCollapse>
@@ -52,22 +52,30 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
       className="z-depth-1"
     >
       <MDBCarouselInner>
-        <MDBCarouselItem itemId="1">
+        <MDBCarouselItem itemId="2">
           <MDBView>
             <img
               className="d-block w-100"
               src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
               alt="First slide"
             />
-          <MDBMask overlay="black-light" />
+          <MDBMask overlay="black-strong" />
           </MDBView>
           <MDBCarouselCaption>
-          <h1>Hello World</h1>
-            <h3 className="h3-responsive">Light mask</h3>
-            <p>First text</p>
+            <div className="c-header text-left">
+              <h3 className="headline-1 text-uppercase">
+                World Health <br/>Organization
+              </h3>
+              <p className="headline-2 ml-1">
+                Current news update of covid 19
+              </p>
+              <MDBBtn target="_blank" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019" color="white" outline>
+                Go to the news page
+              </MDBBtn>
+            </div>
           </MDBCarouselCaption>
         </MDBCarouselItem>
-        <MDBCarouselItem itemId="2">
+        <MDBCarouselItem itemId="3">
           <MDBView>
             <img
               className="d-block w-100"
@@ -76,23 +84,32 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
             />
           <MDBMask overlay="black-strong" />
           </MDBView>
-          <MDBCarouselCaption>
-            <h3 className="h3-responsive">Strong mask</h3>
-            <p>Second text</p>
+          <MDBCarouselCaption className="margin-bottom">
+            <h3 className="headline-1 text-uppercase">CNN news of covid 19</h3>
+            <p>CNN is an American news-based pay television channel owned by AT&T's WarnerMedia. </p>
+            <MDBBtn target="_blank" href="https://edition.cnn.com/world/live-news/coronavirus-outbreak-03-30-20-intl-hnk/index.html" outline color="white">
+              Go to cnn page
+            </MDBBtn>
           </MDBCarouselCaption>
         </MDBCarouselItem>
-        <MDBCarouselItem itemId="3">
+        <MDBCarouselItem itemId="1">
           <MDBView>
             <img
               className="d-block w-100"
               src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg"
               alt="Third slide"
             />
-          <MDBMask overlay="black-slight" />
+          <MDBMask overlay="black-strong" />
           </MDBView>
-          <MDBCarouselCaption>
-            <h3 className="h3-responsive">Slight Mast</h3>
-            <p>Third text</p>
+          <MDBCarouselCaption className="margin-bottom">
+            <h3 className="headline-1 text-uppercase">Every prayer counts</h3>
+            <p>We need to keep on praying for the whole country and for the whole world. God bless to you all and 
+              be safe. Below is where you can request prayers or pray with us. Everyone needs our prayers, the doctors, 
+              the frontliners, the nurses and everyone that is helping to treat this pandemic.
+            </p>
+            <MDBBtn outline color="white">
+              Pray with us
+            </MDBBtn>
           </MDBCarouselCaption>
         </MDBCarouselItem>
       </MDBCarouselInner>
