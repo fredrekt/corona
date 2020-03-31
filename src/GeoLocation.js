36d49bd5bc79bbcda4d74333243e803e8fa5a290
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-04-01 06:45:43 +0800

    added hidden to the dom (displaying long & lat)

--- src/GeoLocation.js
+++ src/GeoLocation.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import Geolocation from "react-geolocation";
 
-export default () => {
-  return (
-    <Geolocation
-      onSuccess={console.log}
-      render={({
-        fetchingPosition,
-        position: { coords: { latitude, longitude } = {} } = {},
-        error,
-        getCurrentPosition
-      }) =>
-        <div hidden>
-          <button onClick={getCurrentPosition}>Get Position</button>
-          {error &&
-            <div>
-              {error.message}
+export default class GeoLocation extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: '',
+      longitute: '',
+      data: []
+    };
+  }
+  render(){
+      return (
+        <Geolocation
+          onSuccess={console.log()}
+          render={
+            ({
+            fetchingPosition,
+            position: { coords: { latitude, longitude } = {} } = {},
+            error,
+            getCurrentPosition
+          }) =>
+            <div hidden>
+              {/* <button onClick={getCurrentPosition}>Get Position</button> */}
+              {error &&
+                <div>
+                  {error.message}
+                </div>}
+              <p>
+                latitude: {latitude} 
+                <br/>
+                longitude: {longitude}
+              </p>
             </div>}
-          <pre>
-            latitude: {latitude} 
-            longitude: {longitude}
-          </pre>
-        </div>}
-    />
-  );
-};
+        />
+      );
+    };
+}
